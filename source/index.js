962052a7508bd17df1c371b89ae378963f3a1521
Chase Moskal <chasemoskal@gmail.com>
2019-05-13 13:10:14 -0700

    main script is now 'register-all.js', improve css

--- source/index.js
+++
@@ -1,6 +0,0 @@
-
-export * from "./components/carouse-pane.js"
-export * from "./components/carouse-carousel.js"
-
-export * from "./toolbox/component.js"
-export * from "./toolbox/register-components.js"
