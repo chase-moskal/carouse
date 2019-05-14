dc0442e205dcf197d1d26db9a1dc8ecc38d0ef1a
Chase Moskal <chasemoskal@gmail.com>
2019-05-13 23:57:28 -0700

    refactor removes carouse-pane
    the carousel now accepts any element

--- source/register-all.js
+++ source/register-all.js
@@ -1,6 +1,5 @@
 
-import {CarousePane} from "./components/carouse-pane.js"
 import {CarouseCarousel} from "./components/carouse-carousel.js"
 import {registerComponents} from "./toolbox/register-components.js"
 
-registerComponents([CarouseCarousel, CarousePane])
+registerComponents([CarouseCarousel])
